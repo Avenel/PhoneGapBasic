86aa5a40612d7377e5259d102adbfe057945648a
Avenel <martin.briewig@googlemail.com>
2013-08-15 10:22:29 +0200

    Added platform: windows phone 8

---
+++ PhoneGapBasic/platforms/wp8/www/js/app.js
@@ -0,0 +1,79 @@
+/**
+  Represents the main application
+**/
+
+App = Ember.Application.create();
+
+App.set('renderedTemplate', 'student');
+
+App.Router.map(function() {
+  this.resource('main', { path: '/' }, function() {
+    this.route('student', { path: '/student' });
+    this.route('aktuelles');
+  });
+});
+
+App.MainStudentRoute = Ember.Route.extend({
+    renderTemplate: function() {
+      this.render('student', {
+        outlet:'content'
+      });     
+      App.set('renderedTemplate', 'student');
+    }
+});
+
+App.MainAktuellesRoute = Ember.Route.extend({
+    renderTemplate: function() {
+      this.render('aktuelles', {
+        outlet:'content'
+      });
+      App.set('renderedTemplate', 'aktuelles');
+    }
+});
+
+// Generate Views
+App.MyView = Mov.ContentView.extend({
+  layout: Ember.Handlebars.compile("<div id='contentWrapper'>{{yield}}</div>"),
+  didInsertElement: function(){
+    this.scheduleMasonry();
+  },
+  scheduleMasonry: (function(){
+    Ember.run.scheduleOnce('afterRender', this, this.applyMasonry);
+  }).observes('App.renderedTemplate'),
+  applyMasonry: function(){
+    // let's jquery mobile render the new content
+    $("#contentWrapper").trigger( "create" );
+  }
+});
+
+App.HeaderView = Mov.HeaderView.extend({
+});
+
+App.FooterView = Mov.FooterView.extend({
+});
+
+App.ListView = Mov.ListView.extend({
+
+});
+
+App.MainView = Mov.PageView.extend({
+    templateName:'main',
+    id: 'main-view',
+    didInsertElement: function() {
+        $.mobile.changePage(this.$());
+    }
+});
+
+
+// Init Page
+$(document).bind('pageinit', function(){
+    console.log('pageinit');
+    var v = App.get('mainView');
+  
+    if (!v) {
+      console.log('main not created');
+      v = App.MainView.create();
+      App.set('mainView',v);
+      //v.append(); 
+    }
+});
